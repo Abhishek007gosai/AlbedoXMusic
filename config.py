9948a9231e16b5b25ef174ecc34d8bde40ed1209
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-05-12 02:34:28 +0530

    Update config.py
    Signed-off-by: Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>

--- config.py
+++ config.py
@@ -5,41 +5,40 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", 0))
-        self.API_HASH = getenv("API_HASH")
+        self.API_ID = 29245477
+        self.API_HASH = "0abc83883262245c90ca337b7a0375c4"
+        self.BOT_TOKEN = ""
+        self.MONGO_URL = "mongodb+srv://musicxrobot:8Up92WwJbgUS39FV@cluster0.ys1jirt.mongodb.net/"
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN")
-        self.MONGO_URL = getenv("MONGO_URL")
-
-        self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
-        self.OWNER_ID = int(getenv("OWNER_ID", 0))
-
+        self.LOGGER_ID = (-1002456565415)
+        self.OWNER_ID = 8667251104
+        
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
         self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 20))
 
-        self.SESSION1 = getenv("SESSION", None)
+        self.SESSION1 = "BQFnJzYANPOCB0yOj2b9gxY4sdgozS1JMu0rk_44EnUC9JrZGVFxHqpX0KqlZiSR0PbBrUcSww5ic8oI0ZwY0SbC1arBZfP95eONbiU_oAGEMNRA4Om1Kg7fE4jCBWiqzgWXn_m2TY9RZyRv3rDo2OaBsj0srTuu2VUukMq4tuZ7kkFwhBPbs_NyZJvcsefoZdmAfHRLnhitxh9w1VoIoMkhbQW8fXAUsYmbKBo28BQEZwPwvM_c4GkLVLTxgmFBZGr2COlHjkc1zQQX4KFLKeazpRduQDSSPTx3EO9TFcBbh2WLMqwlaZImFj4QdNMxyb7H9sUvx_SDCVCwxFSHQ5ZeDyoA9AAAAAICq5w1AA"
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/fallenx")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AnimeNexusNetwork/160")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Anime_Chatting_Groups")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
     
-        self.THUMB_GEN: bool = getenv("THUMB_GEN", "True").lower() == "true"
+        self.THUMB_GEN: bool = getenv("THUMB_GEN", "False").lower() == "true"
         self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", "True").lower() == "true"
 
         self.LANG_CODE = getenv("LANG_CODE", "en")
 
         self.COOKIES_URL = [
-            url for url in getenv("COOKIES_URL", "").split(" ")
+            url for url in getenv("COOKIES_URL", "https://batbin.me/conjugation").split(" ")
             if url and "batbin.me" in url
         ]
-        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://files.catbox.moe/1cdfqm.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/1cdfqm.jpg")
+        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/1cdfqm.jpg")
 
     def check(self):
         missing = [
