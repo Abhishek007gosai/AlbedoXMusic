312ce177d3326ba7c37dd67084331a039bfb72c3
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-05-13 06:57:00 +0530

    Update app.py
    Signed-off-by: Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def health():
     return "ALIVE", 200
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
+    port = int(os.getenv("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
